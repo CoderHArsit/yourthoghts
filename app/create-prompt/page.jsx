b3f9353abc82125a6d04938b83c9fa40d90ea688
CoderHArsit <hs8449865663@gmail.com>
2024-03-09 23:05:17 +0530

    crud functionalities added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import {  useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
+import { reactProductionProfiling } from '@next.config';
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const {data: session}=useSession();
  const [submitting,setSubmitting]=useState(false);
  const[post,setPost]=useState({
   prompt:"",
@@ -14,6 +17,28 @@ const CreatePrompt = () => {
  });
 
  const createPrompt = async(e)=>{
+   e.preventDefault();//prevents browser from reloading default variables
+   setSubmitting(true);
+  
+   try{
+    const response = await fetch('/api/prompt/new',{
+      method:'POST',
+      body:JSON.stringify({
+        prompt:post.prompt,
+        userId:session?.user.id,
+        tag: post.tag
+      })
+    })
+    if(response.ok){
+         router.push('/');
+    }
+   }
+   catch(error){
+    console.log(error);
+   }
+   finally{
+    setSubmitting(false);
+   }
 
  }
   return (
